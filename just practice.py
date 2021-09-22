b22b8b45fdc02ce04674452185aee029031df536
unknown <you@example.com>
2021-09-22 18:04:54 +0500

    updated some example

--- just practice.py
+++ just practice.py
@@ -1,16 +1,16 @@
-def boi(a,b):
-
-    if a > b :
+def boi(a, b):
+    if a > b:
         print("the largest is a =  ", a)
 
     else:
         print("the largest is  b =   ", b)
 
-boi(12,18)
+
+boi(12, 18)
 
 
 def peaky(x, y, z):
-    if x > y and x > z :
+    if x > y and x > z:
         print("the largest among three is  =   ", x)
     elif y > x and y > z:
         print("The largest value among three is =  ", y)
@@ -23,12 +23,12 @@ peaky(13, 17, 19)
 
 
 def str_length(length):
-
     c = 0
     for i in length:
         c += 1
 
     print("the length of the string is  ", c)
+
 
 length = "Ronaldo"
 
@@ -38,23 +38,22 @@ print(str_length(length))
 def vowel(x):
     n = ""
     if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u':
-        print("vowel",x)
+        print("vowel", x)
     else:
         print("this is not vowel")
+
 
 n = input("enter the string ")
 print(vowel(n))
 
 
 def translate(word):
-
     new_word = ""
     consonant = "bcdefghklmnopqrstuvwxyzBCDEFGHKLMNOPQRSTUVWXYZ"
 
-
     for i in word:
         if i in consonant:
-            new_word += i+'o'+ i
+            new_word += i + 'o' + i
         else:
             new_word += i
     return new_word
@@ -68,14 +67,14 @@ def boi(sum):
     lst = 0
 
     for i in sum:
-         lst += i
+        lst += i
     return lst
 
 
 def multiply(sum):
     lst = 1
     for i in sum:
-         lst = lst * i
+        lst = lst * i
     return lst
 
 
@@ -84,14 +83,16 @@ print(multiply([1, 2, 3, 4, 5]))
 
 
 def reverse(x):
-     return x [::-1]
-x = ("fool")
+    return x[::-1]
+
+
+x = "fool"
 print(reverse(x))
 
 
 def plaindrome(string):
+    return string == string[:: -1]
 
-    return string == string [:: -1]
 
 print(plaindrome("abc"))
 print(plaindrome("radar"))
@@ -103,6 +104,19 @@ def member(y, a):
         return False
     return y == a[0] or member(y, a[1:])
 
-print(member(12,["bass",12]))
-print(member(12,[12,'a']))
+
+print(member(12, ["bass", 12]))
+print(member(12, [12, 'a']))
 print(member(4, ['a,b,c']))
+
+
+def generate_char(x, str):
+    result = ''
+    for i in range(x):
+        result += str
+    return result
+
+
+print(generate_char(5, '&'))
+print(generate_char(4, '#'))
+print(generate_char(3, '@'))
